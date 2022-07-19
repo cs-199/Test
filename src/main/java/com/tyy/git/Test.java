97b28c79d42194de483cf47f4a5b66b0ef38260d
cs <chens44@chinatelecom.cn>
2022-07-19 09:48:43 +0800

    hot-fit second commit

--- src/main/java/com/tyy/git/Test.java
+++ src/main/java/com/tyy/git/Test.java
@@ -5,6 +5,6 @@ public class Test {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
-        System.out.println("hot-fix");
+        System.out.println("hot-fix hot-fix");
     }
 }
